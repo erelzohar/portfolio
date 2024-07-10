9111c64e28f45f68d12c8a55ad4e99ab66ee50e1
erelzohar <zoharerel@gmail.com>
2024-07-10 16:41:54 +0300

    added git link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,4 +1,4 @@
-import { Call, Email, LinkedIn, WhatsApp } from "@mui/icons-material";
+import { Call, Email, LinkedIn, WhatsApp, GitHub } from "@mui/icons-material";
 
 export default function SocialLinks() {
     return <div className="SocialLinks">
@@ -6,5 +6,6 @@ export default function SocialLinks() {
         <a href="mailto: zoharerel@gmail.com" target="_self"><Email /> zoharerel@gmail.com</a>
         <a href="https://www.linkedin.com/in/erel-zohar-0a15b6216" target="blank"><LinkedIn /></a>
         <a href="https://wa.me/972584006014" target="blank"><WhatsApp /></a>
+        <a href="https://github.com/erelzohar" target="blank"><GitHub /></a>
     </div>
 }
