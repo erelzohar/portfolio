58a5dbbb4653400dea780883fd87c31b8f351177
erelzohar <zoharerel@gmail.com>
2025-02-16 20:51:16 +0200

    last try

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
   About,
   Contact,
@@ -16,38 +16,41 @@ const App = () => {
   return (
     <div className="relative z-0">
       <BrowserRouter>
-        {window.location.pathname === "/privacy-policy" ? <PrivacyPolicy /> : <>
-          <div>
-            <Navbar />
-            <Hero />
-          </div>
-
-          <div className="bg-about bg-cover bg-center bg-no-repeat">
-            <About />
-          </div>
-
-          <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
-            <Tech />
-          </div>
-
-          <div
-            className="bg-experience bg-cover bg-center bg-no-repeat 
-            rounded-tl-[150px] rounded-br-[150px]">
-            <div
-              className="bg-experienceLight bg-cover bg-center 
-            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
-              <Experience />
+        <Routes>
+          <Route path="/" element={<>
+            <div>
+              <Navbar />
+              <Hero />
             </div>
-          </div>
-          <div className="relative z-0">
-            <Contact />
-            <SocialLinks />
-          </div>
-          <SpeedDialComponent />
-        </>
-        }
 
+            <div className="bg-about bg-cover bg-center bg-no-repeat">
+              <About />
+            </div>
 
+            <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
+              <Tech />
+            </div>
+
+            <div
+              className="bg-experience bg-cover bg-center bg-no-repeat 
+            rounded-tl-[150px] rounded-br-[150px]">
+              <div
+                className="bg-experienceLight bg-cover bg-center 
+            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
+                <Experience />
+              </div>
+            </div>
+            <div className="relative z-0">
+              <Contact />
+              <SocialLinks />
+            </div>
+            <SpeedDialComponent />
+          </>
+          } />
+
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+        </Routes>
       </BrowserRouter>
     </div>
   );
